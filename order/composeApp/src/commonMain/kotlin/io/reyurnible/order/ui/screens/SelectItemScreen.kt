9b34909ac8d50eaa1b818d71a3cf3cae152069a1
reyurnible <reyurnible@gmail.com>
2024-05-26 19:43:35 +0900

    :+1: Add card buttons

--- order/composeApp/src/commonMain/kotlin/io/reyurnible/order/ui/screens/SelectItemScreen.kt
+++ order/composeApp/src/commonMain/kotlin/io/reyurnible/order/ui/screens/SelectItemScreen.kt
@@ -10,15 +10,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import order.composeapp.generated.resources.Res
@@ -60,6 +58,19 @@ fun ItemRow(
                 Text("商品名: Item $index")
                 Spacer(modifier = Modifier.padding(8.dp))
                 Text("価格: ${index * 1000}円")
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("-")
+                    }
+                    Text("1", Modifier.padding(8.dp))
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("+")
+                    }
+                    Spacer(modifier = Modifier.fillMaxWidth().weight(1f))
+                    Button(onClick = { /*TODO*/ }) {
+                        Text("カートに追加")
+                    }
+                }
             }
         }
     }
