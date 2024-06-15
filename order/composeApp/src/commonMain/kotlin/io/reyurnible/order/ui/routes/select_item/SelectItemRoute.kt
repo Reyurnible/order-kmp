1527affc26b9ea49d9215632e57f0148d644dc61
reyurnible <reyurnible@gmail.com>
2024-06-15 16:43:59 +0900

    :+1: Add select item viewmodel

---
+++ order/composeApp/src/commonMain/kotlin/io/reyurnible/order/ui/routes/select_item/SelectItemRoute.kt
@@ -0,0 +1,24 @@
+package io.reyurnible.order.ui.routes.select_item
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.reyurnible.order.ui.screens.OrderCompleteScreen
+import io.reyurnible.order.ui.screens.OrderConfirmationScreen
+import io.reyurnible.order.ui.screens.SelectItemScreen
+
+@Composable
+fun SelectItemRoute(
+    orderViewModel: SelectItemViewModel,
+) {
+    val uiState = orderViewModel.uiState.collectAsStateWithLifecycle()
+
+    when (val state = uiState.value) {
+        is SelectItemUiState.SelectItem -> {
+            SelectItemScreen(
+                onOrderConfirmButtonClicked = {
+                    // TODO Implement
+                }
+            )
+        }
+    }
+}
