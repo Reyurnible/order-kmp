d9896b28a185185edecc5d8f38ac103518aa723a
reyurnible <reyurnible@gmail.com>
2024-06-15 17:30:25 +0900

    :recycle: Rename destination

--- order/composeApp/src/commonMain/kotlin/io/reyurnible/order/ui/OrderAppDestination.kt
+++ order/composeApp/src/commonMain/kotlin/io/reyurnible/order/ui/OrderAppDestination.kt
@@ -1,4 +1,4 @@
-package io.reyurnible.order
+package io.reyurnible.order.ui
 
 import order.composeapp.generated.resources.Res
 import order.composeapp.generated.resources.app_name
@@ -8,7 +8,10 @@ import order.composeapp.generated.resources.order_history__title
 import order.composeapp.generated.resources.select_item__title
 import org.jetbrains.compose.resources.StringResource
 
-enum class OrderScreens(val title: StringResource) {
+/**
+ * Destination of OrderApp Navigation.
+ */
+enum class OrderAppDestination(val title: StringResource) {
     Start(title = Res.string.app_name),
     SelectItem(title = Res.string.select_item__title),
     OrderConfirmation(title = Res.string.order_confirmation__title),
--- order/composeApp/src/commonMain/kotlin/io/reyurnible/order/ui/routes/order/OrderRoute.kt
+++ order/composeApp/src/commonMain/kotlin/io/reyurnible/order/ui/routes/order/OrderRoute.kt
@@ -1,12 +1,9 @@
 package io.reyurnible.order.ui.routes.order
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import io.reyurnible.order.OrderScreens
 import io.reyurnible.order.ui.screens.OrderCompleteScreen
 import io.reyurnible.order.ui.screens.OrderConfirmationScreen
-import io.reyurnible.order.ui.screens.SelectItemScreen
 
 @Composable
 fun OrderRoute(
