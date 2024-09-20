fdb9679716e8a8fe728d3ddfc92a9cddad42fe0b
reyurnible <reyurnible@gmail.com>
2024-09-21 00:51:17 +0900

    :+1: Add endpoint interface

--- order/shared/src/commonMain/kotlin/io/reyurnible/api/endpoints/menus/MenuEndPoints.kt
+++ order/shared/src/commonMain/kotlin/io/reyurnible/api/endpoints/menus/MenuEndPoints.kt
@@ -2,11 +2,12 @@ package io.reyurnible.api.endpoints.menus
 
 import io.reyurnible.api.endpoints.ApiHttpMethod
 import io.reyurnible.api.endpoints.EndPoint
+import io.reyurnible.api.endpoints.EndPoints
 
-interface MenuEndPoints {
-    @EndPoint(method = ApiHttpMethod.GET, path = "/menus")
+interface MenuEndPoints : EndPoints {
+    @EndPoint(method = ApiHttpMethod.GET, path = "/api/menus")
     suspend fun getList(): List<MenuResponse>
 
-    @EndPoint(method = ApiHttpMethod.GET, path = "/menus/{id}")
+    @EndPoint(method = ApiHttpMethod.GET, path = "/api/menus/{id}")
     suspend fun get(id: String): MenuResponse?
 }
